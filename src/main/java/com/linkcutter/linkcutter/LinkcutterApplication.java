443a44e3769f9cea82b4424d691d159156fac315
plaschinskij nikolaj <niccneimi@gmail.com>
2025-10-30 01:07:29 +0300

    init commit

---
+++ src/main/java/com/linkcutter/linkcutter/LinkcutterApplication.java
@@ -0,0 +1,13 @@
+package com.linkcutter.linkcutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkcutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkcutterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkcutter/linkcutter/LinkcutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkcutter.linkcutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkcutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
